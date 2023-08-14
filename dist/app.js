3adfa5083c9e353b4e4e00c148fe28790d0f2fec
Jatin singh <jatinsingh10001@gmail.com>
2023-08-14 22:27:34 +0530

    initial commit

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,6 @@
 "use strict";
 const getUsername = document.querySelector("#user");
-const formSubmit = document.querySelector(".form");
+const formSubmit = document.querySelector("#form");
 const mainContainer = document.querySelector(".main-container");
 const myCustomFetcher = async (url, obj) => {
     const res = await fetch(url, obj);
@@ -24,8 +24,29 @@ const fetchUserData = (url) => {
     myCustomFetcher(url, {}).then((userInfo) => {
         for (const singleUser of userInfo) {
             showResult(singleUser);
-            console.log("login" + singleUser.id);
         }
     });
 };
 fetchUserData("https://api.github.com/users");
+formSubmit.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const searchTerm = getUsername.value.toLowerCase();
+    try {
+        const url = "https://api.github.com/users";
+        const allUserData = await myCustomFetcher(url, {});
+        const matchingUser = allUserData.filter((user) => {
+            return user.login.toLowerCase().includes(searchTerm);
+        });
+        mainContainer.innerHTML = "";
+        if (matchingUser.length === 0) {
+            mainContainer?.insertAdjacentHTML("beforeend", `<P class="empty-msg">No matching users found.</P>`);
+        }
+        else {
+            for (const singleUser of matchingUser)
+                showResult(singleUser);
+        }
+    }
+    catch (error) {
+        throw new Error("something wrong in search Functionality");
+    }
+});
